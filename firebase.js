b0252b8b9492279f46b7b5ec46681689f9ac1008
Ayushman Thakur <ayushman4544@gmail.com>
2026-03-23 12:28:27 +0530

    Delete Firebase configuration from firebase.js
    Removed Firebase configuration and initialization.

--- firebase.js
+++ firebase.js
@@ -1,9 +1,1 @@
-const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  appId: "YOUR_APP_ID"
-};
 
-firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
